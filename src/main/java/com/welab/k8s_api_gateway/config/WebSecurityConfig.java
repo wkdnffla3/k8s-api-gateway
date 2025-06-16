64a4aa9fcd564c4edc1b25df132a65e5106f50fc
wkdnffla3 <wkdnffla3@gmail.com>
2025-06-16 13:40:09 +0900

    asd

--- src/main/java/com/welab/k8s_api_gateway/config/WebSecurityConfig.java
+++ src/main/java/com/welab/k8s_api_gateway/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.welab.k8s_api_gateway.config;
 
+import com.welab.k8s_api_gateway.security.exception.RestAccessDeniedHandler;
+import com.welab.k8s_api_gateway.security.exception.RestAuthenticationEntryPoint;
 import com.welab.k8s_api_gateway.security.filter.JwtAuthenticationFilter;
 import com.welab.k8s_api_gateway.security.jwt.JwtTokenValidator;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,8 @@ import java.util.List;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final RestAuthenticationEntryPoint authenticationEntryPoint;
+    private final RestAccessDeniedHandler accessDeniedHandler;
     private final JwtTokenValidator jwtTokenValidator;
     @Bean
     public SecurityFilterChain applicationSecurity(HttpSecurity http) throws Exception {
@@ -31,11 +35,16 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .securityMatcher("/**") // map current config to given resource path
                 .sessionManagement(sessionManagementConfigurer
-                        -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).formLogin(AbstractHttpConfigurer::disable)
+                        -> sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .addFilterBefore(
                         new JwtAuthenticationFilter(jwtTokenValidator),
                         UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((exceptionConfig) ->
+                exceptionConfig
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler))
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/api/user/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
